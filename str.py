a01eb5959c425ba8d2977ac38fabeb624392a43a
Antony <you@example.com>
2023-03-24 11:12:50 +0200

    Change

---
+++ str.py
@@ -0,0 +1,9 @@
+a = 'Anton'
+b = 'My name is ' + a + ' Pavluhin'
+b_2 = f'My name is {a} Pavluhin'
+b_3 = 'My name is {} Pavluhin'.format(a)
+b_4 = 'My name is %s Pavluhin' % a
+print(b)
+print(b_2)
+print(b_3)
+print(b_4)
